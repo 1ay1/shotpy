4313ec11652283ed01e00305cee7e52cc9016731
Ayush <Ayush>
2017-08-23 16:24:14 +0530

    init

--- shotpy.py
+++ shotpy.py
@@ -7,27 +7,27 @@ import webbrowser
 
 CLIENT_ID = "6db1a5668074579"
 
-if(len(sys.argv) > 2):
-    print("Usage: shotpy <filename>/<path to file>")
-    print("Or: just shotpy, if you want to upload a screensot.")
+if (len(sys.argv) > 2):
+     print("Usage: shotpy <filename>/<path to file>")
+     print("Or: just shotpy, if you want to upload a screensot.")
 
-if(len(sys.argv) == 2):
-    if(len(sys.argv) == 2):
-        argv1 = sys.argv[1]
-        porn = '/' in sys.argv[1]
-        if(porn == False):
-            PATH = './' + sys.argv[1]
-            im = pyimgur.Imgur(CLIENT_ID)
-            uploaded_image = im.upload_image(PATH, title="Uploaded with shotpy :)")
-            s.call(['notify-send', '1 Picture Uploaded Successfully :)', uploaded_image.link])
-            pyperclip.copy(uploaded_image.link)
-            webbrowser.open_new_tab(uploaded_image.link)
+if (len(sys.argv) == 2):
+     if (len(sys.argv) == 2):
+          argv1 = sys.argv[1]
+          porn = '/' in sys.argv[1]
+          if(porn == False):
+              PATH = './' + sys.argv[1]
+              im = pyimgur.Imgur(CLIENT_ID)
+              uploaded_image = im.upload_image(PATH, title="Uploaded with shotpy :)")
+              s.call(['notify-send', '1 Picture Uploaded Successfully :)', uploaded_image.link])
+              pyperclip.copy(uploaded_image.link)
+              webbrowser.open_new_tab(uploaded_image.link)
 
-        if (porn == True):
-            PATH = sys.argv[1]
-            im = pyimgur.Imgur(CLIENT_ID)
-            uploaded_image = im.upload_image(PATH, title="Uploaded with shotpy :)")
-            s.call(['notify-send', '1 Picture Uploaded Successfully :)', uploaded_image.link])
-            pyperclip.copy(uploaded_image.link)
-            webbrowser.open_new_tab(uploaded_image.link)
+          if (porn == True):
+              PATH = sys.argv[1]
+              im = pyimgur.Imgur(CLIENT_ID)
+              uploaded_image = im.upload_image(PATH, title="Uploaded with shotpy :)")
+              s.call(['notify-send', '1 Picture Uploaded Successfully :)', uploaded_image.link])
+              pyperclip.copy(uploaded_image.link)
+              webbrowser.open_new_tab(uploaded_image.link)
 
